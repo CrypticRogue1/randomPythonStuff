0a78733027884074d1d1218333c8a456f0c81207
Cryptic_Rogue1 <53357237+CrypticRogue1@users.noreply.github.com>
2020-05-21 19:46:57 -0500

    Indentation is weird.
    You know. There's this thing I read about called elif. It's kind of weird. I should look that up later.

---
+++ finallyIfAndElse.py
@@ -0,0 +1,14 @@
+# Example in book
+
+name = "Mary"
+password = "swordfish"
+
+if name == "Mary":
+    print("Hello, Mary.")
+    if password == "swordfish":
+        print("Access Granted.")
+    else:
+        print("Wrong Password.")
+
+# I want to automate this so that later it can literally be like, you enter your password
+# and then it checks if it's correct and does the stuff in this program.
